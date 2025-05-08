102e497d8edc59d56e5d685988b3a2aa55cf6c4f
Zian Chen <doomon@gmail.com>
2025-05-08 19:59:39 -0400

    feat: update

--- utils/WebSocketManager/WebSocketManager.ts
+++ utils/WebSocketManager/WebSocketManager.ts
@@ -26,6 +26,7 @@ export class WebSocketManager {
 
 		this.currentProfile = profile;
 		const url = import.meta.env.VITE_WS_URL || 'ws://localhost:4000';
+		console.log(import.meta.env.VITE_WS_URL);
 		this.ws = new WebSocket(url);
 		this.setupEventHandlers();
 	}
